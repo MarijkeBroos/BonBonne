18e2945e18a9dbc500c5e975c9927cc35c13f76b
Marijke Broos <broos.marijke@gmail.com>
2019-03-14 16:48:30 +0100

    Updates

---
+++ precache-manifest.ff70f0304ddb58ad1ceb5d1eb93285d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8c28c468b5f250f853263f06a3b1836",
+    "url": "/Restaurant-Api/static/media/restaurant.e8c28c46.jpeg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Restaurant-Api/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c0f570a8a1764f02e175",
+    "url": "/Restaurant-Api/static/js/runtime~main.0e334482.js"
+  },
+  {
+    "revision": "78911fe38a6ebaafee58",
+    "url": "/Restaurant-Api/static/js/main.31ff05e8.chunk.js"
+  },
+  {
+    "revision": "79d6572e3d0d9e8c05cf",
+    "url": "/Restaurant-Api/static/js/2.931077ea.chunk.js"
+  },
+  {
+    "revision": "78911fe38a6ebaafee58",
+    "url": "/Restaurant-Api/static/css/main.df2ed3df.chunk.css"
+  },
+  {
+    "revision": "2c7ba54da2208b8d4a52cc2c5fdc46df",
+    "url": "/Restaurant-Api/index.html"
+  }
+];
