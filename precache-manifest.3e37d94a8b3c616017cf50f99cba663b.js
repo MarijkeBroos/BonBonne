565793bf033a6d150a39704e75f7c041e4d9dbbc
Marijke Broos <broos.marijke@gmail.com>
2019-03-17 11:05:51 +0100

    Updates

---
+++ precache-manifest.3e37d94a8b3c616017cf50f99cba663b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8c28c468b5f250f853263f06a3b1836",
+    "url": "/BonBonne/static/media/restaurant.e8c28c46.jpeg"
+  },
+  {
+    "revision": "b7ae4dc81816fbd1c5a4",
+    "url": "/BonBonne/static/js/runtime~main.c07618cb.js"
+  },
+  {
+    "revision": "f5c8ba3ac29736871292",
+    "url": "/BonBonne/static/js/main.3cd406f7.chunk.js"
+  },
+  {
+    "revision": "62352146ed2245ed119a",
+    "url": "/BonBonne/static/js/2.0311b00f.chunk.js"
+  },
+  {
+    "revision": "f5c8ba3ac29736871292",
+    "url": "/BonBonne/static/css/main.92a5a5fd.chunk.css"
+  },
+  {
+    "revision": "5d532a21da6b6d1b009e9462777b09ca",
+    "url": "/BonBonne/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Restaurant-Api/precache-manifest.ff70f0304ddb58ad1ceb5d1eb93285d0.js"
+  "/BonBonne/precache-manifest.3e37d94a8b3c616017cf50f99cba663b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Restaurant-Api/index.html", {
+workbox.routing.registerNavigationRoute("/BonBonne/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
